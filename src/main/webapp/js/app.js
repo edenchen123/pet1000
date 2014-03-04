cd56efa344e8916abbc994cedd868b378e08ad77
edenchen123 <eden.chen@hbo.com>
2014-03-04 20:24:01 +0800

    init

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,18 +3,20 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('pet1000', [
-  'ngRoute',
-  'pet1000.filters',
-  'pet1000.services',
-  'pet1000.directives',
-  'pet1000.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {templateUrl: 'views/home.html'});
-  $routeProvider.when('/petServices', {templateUrl: 'views/pet-services.html'});
-  $routeProvider.when('/petAdoption', {templateUrl: 'views/pet-adoption.html'});
-  $routeProvider.when('/petVIP', {templateUrl: 'views/pet1000-vip.html'});
-  $routeProvider.when('/petOffers', {templateUrl: 'views/pet-spec-offers.html'});
-  $routeProvider.when('/petStore', {templateUrl: 'views/pet-stores.html'});
-  $routeProvider.otherwise({redirectTo: '/home'});
-}]);
+        'ngRoute',
+        'ui.route',
+        'pet1000.filters',
+        'pet1000.services',
+        'pet1000.directives',
+        'pet1000.controllers'
+    ]).
+    config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
+        $routeProvider.when('/home', {templateUrl: 'views/home.html'});
+        $routeProvider.when('/petServices', {templateUrl: 'views/pet-services.html'});
+        $routeProvider.when('/petAdoption', {templateUrl: 'views/pet-adoption.html'});
+        $routeProvider.when('/petVIP', {templateUrl: 'views/pet1000-vip.html'});
+        $routeProvider.when('/petOffers', {templateUrl: 'views/pet-spec-offers.html'});
+        $routeProvider.when('/petStore', {templateUrl: 'views/pet-stores.html'});
+        $routeProvider.otherwise({redirectTo: '/home'});
+
+    }]);
