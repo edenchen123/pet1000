9db9d6f406382d462a3581a062d51aaa9784c138
echen <eden.chen@hbo.com>
2014-03-12 16:58:00 +0800

    New changelist

--- src/main/java/com/pet1000/services/PetServicesImpl.java
+++ src/main/java/com/pet1000/services/PetServicesImpl.java
@@ -3,7 +3,7 @@ package com.pet1000.services;
 public class PetServicesImpl implements PetServices {
 
     public String getAllAvaServices(String userName) throws Exception {
-           return userName;
+           return "{'name':"+userName+"}";
     }
 
 
